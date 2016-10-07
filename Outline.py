9da1ccf24afbc862ffac37d399e8cc842d139494
Pentabyteman <nils@brennbach.de>
2016-10-07 14:22:02 +0200

    Updated Outline.py
    class Robot ist nun sogar funktionstüchtig.
    
    Wir brauchen eine gute Idee, um die Felder und die FeldAttribute zu klären!

--- Outline.py
+++ Outline.py
@@ -2,35 +2,63 @@
 
 #Hier stehen in einem Outline die main-functions eines Roboters
 
-class robot:
-    robot_rotation = 0 #Wie Himmelsrichtungen: 1=N, 2=O, 3=S, 4=W
-    robot_position = [0, 0] #Koordinatensystem mit x,y
-
+class Field():
+    passable = True
+    kind = "" #type is a key word ;(
+    team = "zero"
+    occupied = "zero"
+    
+    
+class Robot():
+    health = 100
+    rotation = 3 #Wie Himmelsrichtungen: 1=N, 2=O, 3=S, 4=W
+    position = [0, 0] #Koordinatensystem mit x,y
+    
+    def get_robot_position():
+        robot_position = position #just be sure so that we are not changing important values
+        return robot_position
+    
+    def get_robot_rotation():
+        robot_rotation = rotation
+        return robot_rotation
+    
     def turn_right(): #Roboter dreht sich nach rechts
-        robot_rotation += 1 #siehe oben, Rechtskurve = +1 (von N nach O etc.)
-        if robot_rotation > 4: 
-            robot_rotation == 0
+        rotation += 1 #siehe oben, Rechtskurve = +1 (von N nach O etc.)
+        if rotation > 4: 
+            rotation == 0
         screen.update() #zeichnet den Screen neu
 
     def turn_left(): #Roboter dreht sich nach links
-        robot_rotation -= 1 #siehe oben, Rechtskurve = +1 (von N nach O etc.)
-        if robot_rotation < 1:
-            robot_rotation==4
+        rotation -= 1 #siehe oben, Rechtskurve = +1 (von N nach O etc.)
+        if rotation < 1:
+            rotation==4
         screen.update() #zeichnet den Screen neu
 
-    def move_forward(): #NEEDS CHECK IF THE RESULTING FIELD IS PASSABLE!
+    def move_forward():
         rotation = self.get_robot_rotation()
         position = self.get_robot_position()
         x = position[0]
         y= position[1]
         if rotation == 0:
-            y += 1
+            if lookAtField(x, y+1).passable == True:
+                y += 1
+            else:
+                print("Hindernis im Weg, Roboter kann sich nicht bewegen")
         elif rotation == 1:
-            x += 1
+            if lookAtField(x+1, y).passable == True:
+                x += 1
+            else:
+                print("Hindernis im Weg, Roboter kann sich nicht bewegen")
         elif rotation == 2:
-            y -= 1
+            if lookAtField(x, y-1).passable == True:
+                y -= 1
+            else:
+                print("Hindernis im Weg, Roboter kann sich nicht bewegen")
         elif rotation == 3:
-            x -= 1
+            if lookAtField(x-1, y).passable == True:
+                x -= 1
+            else:
+                print("Hindernis im Weg, Roboter kann sich nicht bewegen")
         robot_position = [x, y]
         screen.update()
 
@@ -40,13 +68,25 @@ class robot:
         x = position[0]
         y = position[1]
         if rotation == 0:
-            y -= 1
+            if lookAtField(x, y-1).passable == True:
+                y -= 1
+            else:
+                print("Hindernis im Weg, Roboter kann sich nicht bewegen")
         elif rotation == 1:
-            x -= 1
+            if lookAtField(x-1, y).passable == True:
+                x -= 1
+            else:
+                print("Hindernis im Weg, Roboter kann sich nicht bewegen")
         elif rotation == 2:
-            y += 1
+            if lookAtField(x, y+1).passable == True:
+                y += 1
+            else:
+                print("Hindernis im Weg, Roboter kann sich nicht bewegen")
         elif rotation == 3:
-            x += 1
+             if lookAtField(x+1, y).passable == True:
+                x += 1
+            else:
+                print("Hindernis im Weg, Roboter kann sich nicht bewegen")
         robot_position = [x, y]
         screen.update()
         
@@ -63,15 +103,50 @@ class robot:
             y += 1
         elif rotation == 3:
             x += 1
-        if lookAtField(x, y).type == opponent:
+        if lookAtField(x, y).occupied == opponent: #need to define opponent
             damage()
         else:
             pass
+        
+#Fehlende functions:
+def update():
+    pass
+#Was bei screen.update alles passieren muss:
+#- die Roboter werden geupdated
+#   - Drehungen werden ausgeführt
+#   - Bewegungen werden ausgeführt
+#   - Lebensleiste wird neu angezeigt
+#- jedes Feld wird geupdated:
+#   -Wenn ein Roboter darauf steht: passable => False, team => robot_team, occupied => robot
+#   -Wenn ein Roboter nicht darauf steht:passable => False, team=> NICHT AENDERN, occupied => "zero"
 
-#missing classes:
-class field:
+def damage():
     
-#missing functions:
-screen.update()
-damage()
-lookAtField()
+
+
+def lookAtField(x, y):
+    field = Field[x, y] #oder so ähnlich, siehe unten.
+    #Wie machen wir das mit den Feldern? Wo wird die ganze Information abgespeichert?? In einer Liste, oder ist jedes Feld ein
+    #Objekt mit den dazugehörigen x und y Werten?
+    def passable():
+        #if object can not be found (je nach Variante andere Syntax) or object can be found and is unpassable:
+            return False
+        #if object can be found and is passable:
+            return True
+    
+    def kind():
+        #if object can not be found (je nach Variante andere Syntax):
+            return "wall"
+        #if object can be found and is a normal field:
+            return "field"
+        #if object can be found and is a obstacle
+            return "obstacle"
+    
+    def occupied():
+        #if red_robot occupies this field
+            return "red_robot"
+        #if blue/robot occupies this field
+            return "blue_robot"
+        #if object can not be found (je nach Variante andere Syntax):
+            return "zero"
+        
